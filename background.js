ed19bc1773e597c3749f518ba39c1320cae54aba
tonysodeep <hynguyen2727@gmail.com>
2024-01-18 18:48:42 +0700

    finished final auto ditect ad while video playing

--- background.js
+++ background.js
@@ -1,14 +1,20 @@
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
-    console.log('tab on updated');
-    if (tab.url && tab.url.includes("youtube.com/watch")) {
-      const queryParameters = tab.url.split("?")[1];
-      const urlParameters = new URLSearchParams(queryParameters);
-  
+  console.log("change over click on new tab");
+  if (tab.url && tab.url.match("https://www.youtube.com/")) {
+    const queryParameters = tab.url.split("?")[1];
+    const urlParameters = new URLSearchParams(queryParameters);
+
+    if (tab.url.includes("watch")) {
+      console.log("watching video ....");
       chrome.tabs.sendMessage(tabId, {
-        type: "NEW",
+        type: "VIDEO_PLAYING",
         videoId: urlParameters.get("v"),
       });
+    } else {
+      console.log("ad homepage ...");
+      chrome.tabs.sendMessage(tabId, {
+        type: "VIDEO_PAUSE",
+      });
     }
-    
-  });
-  
+  }
+});
--- contentScript.js
+++ contentScript.js
@@ -1,52 +1,45 @@
 (() => {
   let youtubeLeftControls, youtubePlayer;
   let currentVideo = "";
+  var skipAdInterval;
+
+  const newVideoLoaded = async () => {
+    console.log("testing skip ad function");
+
+    youtubePlayer = document.getElementsByClassName("video-stream")[0];
+
+    skipAdInterval = setInterval(() => {
+      if (document.getElementsByClassName("ytp-ad-text").length != 0) {
+        console.log("ad dectivced");
+        skipAds();
+      } else {
+        console.log("still checking.....");
+      }
+    }, 500);
+  };
+
+  const skipAds = () => {
+    youtubePlayer.currentTime = youtubePlayer.duration;
+    const skipBtn = document.querySelector(
+      ".ytp-ad-skip-button-modern.ytp-button"
+    );
+
+    if (skipBtn) {
+      skipBtn.click();
+    }
+  };
 
   chrome.runtime.onMessage.addListener((obj, sender, response) => {
     const { type, value, videoId } = obj;
 
-    if (type === "NEW") {
-      currentVideo = videoId;
-      setTimeout(newVideoLoaded, 1000);
+    currentVideo = videoId;
+    if (type === "VIDEO_PLAYING") {
+      newVideoLoaded();
+    }
+
+    if (type === "VIDEO_PAUSE") {
+      console.log("intervalid", skipAdInterval);
+      clearInterval(skipAdInterval);
     }
   });
-
-  const newVideoLoaded = () => {
-    console.log("testing skip ad function");
-
-    const video = document.querySelector("video");
-    const targetElement = document.getElementsByClassName("ytp-ad-text");
-    const config = { attributes: true, childList: true, subtree: true };
-
-    // let callback = (mutationList, observer) => {
-    //   for (const mutation of mutationList) {
-    //     if (mutation.type === "childList") {
-    //       console.log("A child node has been added or removed.");
-    //     } else if (mutation.type === "attributes") {
-    //       console.log(`The ${mutation.attributeName} attribute was modified.`);
-    //     }
-    //   }
-    let skipInterval = setInterval(() => {
-      const videoAdExists =
-        document.getElementsByClassName("ytp-ad-text").length;
-      if (videoAdExists != 0) {
-        console.log("this video have ad");
-        video.currentTime = video.duration;
-        const skipBtn = document.querySelector(
-          ".ytp-ad-skip-button-modern.ytp-button"
-        );
-        if (skipBtn) {
-          skipBtn.click();
-        }
-      } else {
-        console.log("this video not have ad");
-        clearInterval(skipInterval);
-      }
-    }, 1000);
-  };
-
-  // const observer = new MutationObserver(callback);
-  // observer.observe(targetElement, config);
-
-  //   newVideoLoaded();
 })();
