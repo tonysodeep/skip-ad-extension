075184386ea25724b75db248970c8d4ea80333f9
tonysodeep <hynguyen2727@gmail.com>
2024-01-19 12:15:10 +0700

    fixed duplicate interval when move to next video

--- contentScript.js
+++ contentScript.js
@@ -1,45 +1,46 @@
-(() => {
-  let youtubeLeftControls, youtubePlayer;
-  let currentVideo = "";
-  var skipAdInterval;
+let youtubeLeftControls, youtubePlayer;
+let currentVideo = "";
+var skipAdInterval;
 
-  const newVideoLoaded = async () => {
-    console.log("testing skip ad function");
+const newVideoLoaded = async () => {
+  console.log("new video loaded");
+  youtubePlayer = document.getElementsByClassName("video-stream")[0];
 
-    youtubePlayer = document.getElementsByClassName("video-stream")[0];
+  if (skipAdInterval) {
+    console.log("old interval", skipAdInterval);
+    clearInterval(skipAdInterval);
+  }
 
-    skipAdInterval = setInterval(() => {
-      if (document.getElementsByClassName("ytp-ad-text").length != 0) {
-        console.log("ad dectivced");
-        skipAds();
-      } else {
-        console.log("still checking.....");
-      }
-    }, 500);
-  };
-
-  const skipAds = () => {
-    youtubePlayer.currentTime = youtubePlayer.duration;
-    const skipBtn = document.querySelector(
-      ".ytp-ad-skip-button-modern.ytp-button"
-    );
-
-    if (skipBtn) {
-      skipBtn.click();
+  skipAdInterval = setInterval(() => {
+    if (document.getElementsByClassName("ytp-ad-text").length != 0) {
+      console.log("ad dectivced");
+      skipAds();
+    } else {
+      console.log("still checking.....");
     }
-  };
+  }, 500);
+};
 
-  chrome.runtime.onMessage.addListener((obj, sender, response) => {
-    const { type, value, videoId } = obj;
+const skipAds = () => {
+  youtubePlayer.currentTime = youtubePlayer.duration;
+  const skipBtn = document.querySelector(
+    ".ytp-ad-skip-button-modern.ytp-button"
+  );
 
-    currentVideo = videoId;
-    if (type === "VIDEO_PLAYING") {
-      newVideoLoaded();
-    }
+  if (skipBtn) {
+    skipBtn.click();
+  }
+};
 
-    if (type === "VIDEO_PAUSE") {
-      console.log("intervalid", skipAdInterval);
-      clearInterval(skipAdInterval);
-    }
-  });
-})();
+chrome.runtime.onMessage.addListener((obj, sender, response) => {
+  console.log("testing skip ad function");
+  const { type, value, videoId } = obj;
+
+  currentVideo = videoId;
+  if (type === "VIDEO_PLAYING") {
+    newVideoLoaded();
+  } else {
+    console.log("current intervalid", skipAdInterval);
+    if (skipAdInterval) clearInterval(skipAdInterval);
+  }
+});
